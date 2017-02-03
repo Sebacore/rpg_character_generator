0a5f03ec35dbb6a191999bb0b5cc5043ca293e3b
Sebacore <cnanderson@gmail.com>
2017-02-03 16:17:40 -0500

    Created luckyroll.py
    Contains the Lucky Roll table.

---
+++ luckyroll.py
@@ -0,0 +1,121 @@
+lucky_roll = [{
+    'roll': 1,
+    'Effect': 'Attack',
+    'Augur': 'Harsh winter: All attack rolls'
+}, {
+    'roll': 2,
+    'Effect': 'Melee',
+    'Augur': 'The bull: Melee attack rolls'
+}, {
+    'roll': 3,
+    'Effect': 'Missle',
+    'Augur': 'Fortunate date: Missile fire attack rolls'
+}, {
+    'roll': 4,
+    'Effect': 'Unarmed',
+    'Augur': 'Raised by wolves: Unarmed attack rolls'
+}, {
+    'roll': 5,
+    'Effect': 'Mounted',
+    'Augur': 'Conceived on horseback: Mounted attack rolls'
+}, {
+    'roll': 6,
+    'Effect': 'Damage',
+    'Augur': 'Born on the battlefield: Damage rolls'
+}, {
+    'roll': 7,
+    'Effect': 'Damage-Melee',
+    'Augur': 'Path of the bear: Melee damage rolls'
+}, {
+    'roll': 8,
+    'Effect': 'Damage-Missile',
+    'Augur': 'Hawkeye: Missile fire damage rolls'
+}, {
+    'roll': 9,
+    'Effect': 'Damage-Zero',
+    'Augur': 'Pack hunter: Attack and damage rolls for 0-level starting weapon'
+}, {
+    'roll': 10,
+    'Effect': 'Skill-All',
+    'Augur': 'Born under the loom: Skill checks (including thief skills)'
+}, {
+    'roll': 11,
+    'Effect': 'Skill-Trap',
+    'Augur': "Fox's cunning: Find/disable traps"
+}, {
+    'roll': 12,
+    'Effect': 'Skill-Secret',
+    'Augur': 'Four-leafed clover: Find secret doors'
+}, {
+    'roll': 13,
+    'Effect': 'Spell-Check',
+    'Augur': 'Seventh son: Spell checks'
+}, {
+    'roll': 14,
+    'Effect': 'Spell-Damage',
+    'Augur': 'The raging storm: Spell damage'
+}, {
+    'roll': 15,
+    'Effect': 'Unholy',
+    'Augur': 'Righteous heart: Turn unholy checks'
+}, {
+    'roll': 16,
+    'Effect': 'Magic-Healing',
+    'Augur': 'Survived the plague: Magical healing*'
+}, {
+    'roll': 17,
+    'Effect': 'Saves',
+    'Augur': 'Lucky sign: Saving throws'
+}, {
+    'roll': 18,
+    'Effect': 'Saves-Traps',
+    'Augur': 'Guardian angel: Savings throws to escape traps'
+}, {
+    'roll': 19,
+    'Effect': 'Saves-Poison',
+    'Augur': 'Survived a spider bite: Saving throws against poison'
+}, {
+    'roll': 20,
+    'Effect': 'Saves-Reflex',
+    'Augur': 'Struck by lightning: Reflex saving throws'
+}, {
+    'roll': 21,
+    'Effect': 'Saves-Fort',
+    'Augur': 'Lived through famine: Fortitude saving throws'
+}, {
+    'roll': 22,
+    'Effect': 'Saves-Will',
+    'Augur': 'Resisted temptation: Willpower saving throws'
+}, {
+    'roll': 23,
+    'Effect': 'AC',
+    'Augur': 'Charmed house: Armor Class'
+}, {
+    'roll': 24,
+    'Effect': 'Init',
+    'Augur': 'Speed of the cobra: Initiative'
+}, {
+    'roll': 25,
+    'Effect': 'HP',
+    'Augur': 'Bountiful harvest: Hit points (applies at each level)'
+}, {
+    'roll': 26,
+    'Effect': 'Crit',
+    'Augur': "Warrior's arm: Critical hit tables**"
+}, {
+    'roll': 27,
+    'Effect': 'Corruption',
+    'Augur': 'Unholy house: Corruption rolls'
+}, {
+    'roll': 28,
+    'Effect': 'Fumbles',
+    'Augur': 'The Broken Star: Fumbles**'
+}, {
+    'roll': 29,
+    'Effect': 'Lang',
+    'Augur': 'Birdsong: Number of languages'
+}, {
+    'roll': 30,
+    'Effect': 'Speed',
+    'Augur': "Wild child: Speed (each +1/-1 = +5'/-5' speed)"
+}]
